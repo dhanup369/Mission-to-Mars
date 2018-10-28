fda0ec2931ddc6ec564b9431830c84f65e4eae5b
Dhanup369 <dhanup369@gmail.com>
2018-10-28 14:58:21 -0700

    new

---
+++ Instructions/app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import mission_to_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://admin:password1@ds143953.mlab.com:43953/mars"
+mongo = PyMongo(app)
+
+
+
+@app.route("/")
+def index():
+    listings = mongo.db.listings.find_one()
+    return render_template("index.html", listings=listings)
+
+
+@app.route("/scrape")
+def scraper():
+    listings = mongo.db.listings
+    listings_data = mission_to_mars.scrape()
+    listings.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
